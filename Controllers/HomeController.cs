acdc606c6eb1cece03115fec43cbea0ef771a8ce
AutonomeSchule <benutzer.mrh.01@gmail.com>
2018-06-14 18:58:37 +0200

    wir beginnen mit sql

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,33 +25,7 @@ namespace AmineV02.Controllers
             return View(model);
 
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-        public IActionResult Test()
-        {
-            ViewData["Message"] = "Testbereich";
-
-            return View();
-        }
-
-        public IActionResult Platz()
-        {
-                ViewData["Message"] = "Unser Ort";
-
-            return View();
-        }
+           
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
