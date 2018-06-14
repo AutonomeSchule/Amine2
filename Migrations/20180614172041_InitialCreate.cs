7c0442ab249a8734f64446c0c7ce587a2bfeadc3
AutonomeSchule <benutzer.mrh.01@gmail.com>
2018-06-14 19:39:42 +0200

    -f für überschreiben

---
+++ Migrations/20180614172041_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AmineV02.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gaeste",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Ankunft = table.Column<DateTime>(nullable: false),
+                    Genre = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gaeste", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gaeste");
+        }
+    }
+}
--- Models/Gaeste.cs
+++ Models/Gaeste.cs
@@ -5,9 +5,18 @@ namespace AMINEV02.Models
     public class Gaeste
     {
         public int ID { get; set; }
-        public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public string Name { get; set; }
+        public DateTime Ankunft { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public int Phonenumber {get;set;}
     }
 }
+
+// beim ersten mal
+//dotnet restore
+//dotnet aspnet-codegenerator controller -name gaste2Controller -m Gaeste -dc AMINEV02Context --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries 
+
+//bei Änderung
+//dotnet aspnet-codegenerator controller -name gaste2Controller -m Gaeste -dc AMINEV02Context --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -f
+
